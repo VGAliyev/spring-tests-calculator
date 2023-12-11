77e5f92d2caa1d1643e1b4c501dda67a7ff427b0
VGAliyev <v.aliev84@yandex.ru>
2023-12-11 21:19:00 +0400

    Initial commit

---
+++ src/main/java/ru/skypro/course2/valiyev/springcalculatortests/SpringCalculatorTestsApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.course2.valiyev.springcalculatortests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCalculatorTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCalculatorTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/course2/valiyev/springcalculatortests/SpringCalculatorTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.course2.valiyev.springcalculatortests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCalculatorTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
